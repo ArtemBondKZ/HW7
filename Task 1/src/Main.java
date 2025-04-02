7036964243a8f755db5a7d67b16aa7f4d3bd523a
Артем <artem.bondatenko.97@gmail.com>
2025-04-02 18:17:27 +0400

    задание 1

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int toadEyesCount = 0;          //Жабьи глаза
@@ -5,6 +7,16 @@ public class Main {
         int ravenBonesCount = 0;        //Кости ворона
         int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
+
+        System.out.println("Введите количество жабьих глаз:");
+        toadEyesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество Слез вурдалака:");
+        ghoulTearsCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество Костей ворона:");
+        ravenBonesCount = new Scanner(System.in).nextInt();
+        System.out.println("Введите количество Пельменей:");
+        dumplingsCount = new Scanner(System.in).nextInt();
+        boolean isCount = true;
         //todo реализовать ввод пользователем кол-ва ингредиентов.
 
         //Рецепты Эликсиров:
@@ -14,5 +26,26 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        if(toadEyesCount >= 3 && ravenBonesCount >= 1){
+            System.out.println("Вы можете сделать Эликсир зоркости:");
+            isCount = true;
+        }
+        if(ravenBonesCount >= 2 && dumplingsCount >=4){
+            System.out.println("Вы можете сделать Эликсир стойкости:");
+            isCount = true;
+        }
+        if(ghoulTearsCount >= 7 && dumplingsCount >=1 && toadEyesCount >=2){
+            System.out.println("Вы можете сделать Эликсир скрытности:");
+            isCount = true;
+        }
+        if(ghoulTearsCount >= 5 && dumplingsCount >=10 && toadEyesCount >=4 && ravenBonesCount >=3){
+            System.out.println("Вы можете сделать запретный эликсир:");
+            isCount = true;
+        }
+        if(isCount == false){
+            System.out.println("Вы ничего не можете сделать:");
+        }
+
+       }
     }
-}
+
